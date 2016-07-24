1f5f9a966afaf38166c45b71262e0befa0917fb4
Shamrikov Sergey <sergey.shamrikov@gmail.com>
2016-07-25 02:00:07 +0300

    page tasks.html

--- js/script.core.js
+++ js/script.core.js
@@ -9,14 +9,18 @@
 			var self = this;
 
 			self.tableToggle();
+			self.dropdown();
 
 		},
 
 		windowLoad: function(){
 
 			var self = this;
+
+			self.proggresBur();
 			
 		},
+
 
 		/**
 		**	table toggle
@@ -31,7 +35,47 @@
 
 		    });
 
-		}
+		},
+
+
+
+		/**
+		**	Dropdown
+		**/
+
+		dropdown: function(){
+
+			$('.dropdown_btn').on('click',function(){
+				$(this).closest('.dropdown_box').toggleClass('opened');
+			});
+
+			$(document).on('click', function(e){
+
+				if(!$(e.target).closest('.dropdown_box').length){
+					$('.dropdown_box').removeClass('opened');
+				}
+
+			});
+
+		},
+
+
+
+		/**
+		**	Proggres Bur
+		**/
+
+		proggresBur: function(){
+
+			$('.proggres_bur').each(function(index, el){
+
+				var percent = $(el).attr('data-val');
+
+				$(el).find('.proggres_bur_val').text(percent+'%');
+				$(el).find('.proggres_bur_inner').animate({'width':percent+'%'},500);
+			});
+
+		},
 
 	}
 
